d096080f7ee58304120ddb18bfeb8b98fe65a8ac
Michal123-p <51889159+Michal123-p@users.noreply.github.com>
2021-04-30 10:40:50 +0200

    Michał branch

--- src/Structures/OR.java
+++ src/Structures/OR.java
@@ -1,5 +1,31 @@
 package Structures;
 
-public class OR {
+public class OR extends Structure {
+    public OR(int startRow, int startCol) {
+        super(startRow, startCol);
+    }
+
+    @Override
+    public int[][] addstruct(int[][] board) {
+        int lp = 3; //liczba przewodnikw;
+        // dwa przewody po 3 przewodniki
+        for (int i = 0 ; i < 3; i+=2)
+            for(int j = 0; j < lp; j++)
+                board[startRow+i][startCol+j] = 1;
+            //wzór bramki
+
+        board[startRow-1][startCol+lp] = 1;
+        board[startRow-1][startCol+lp+1] = 1;
+        board[startRow+3][startCol+lp] = 1;
+        board[startRow+3][startCol+lp+1] = 1;
+        board[startRow+2][startCol+lp+2] = 1;
+        board[startRow][startCol+lp+2] = 1;
+
+        for(int i =lp+1; i < 2*lp+3 ; i++)
+            board[startRow+1][startCol+i] = 1;
+
+        return board;
+
+    }
 
 }
--- src/Wireworld.java
+++ src/Wireworld.java
@@ -1,4 +1,5 @@
 import Structures.Dioda;
+import Structures.OR;
 import Structures.Structure;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,10 +9,12 @@ public class Wireworld {
         public static void main(String[] args) {
             Structure s = new Structure(2,2);
             Dioda s1 = new Dioda(3,2, "Reversed");
+            OR s2 = new OR(2,2);
 
             int board [] [] = new int[10][30];
-            board = s1.addstruct(board);
-            s.addstruct(board);
+            //board = s1.addstruct(board);
+            //s.addstruct(board);
+            s2.addstruct(board);
             System.out.println(Arrays.deepToString(board).replace("], ", "]\n"));;
         }
 }
