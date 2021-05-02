6b294bf755c51002ae8fcb15d972da1c218caf7e
Michal123-p <51889159+Michal123-p@users.noreply.github.com>
2021-05-02 18:34:40 +0200

    skończone struktury

---
+++ src/Structures/Structure_list.java
@@ -0,0 +1,20 @@
+package Structures;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Structure_list  implements Iterable<Structure> {
+
+    private ArrayList<Structure> struktury;
+
+    public Structure_list(){ struktury = new ArrayList<Structure>(); }
+
+    public void add(Structure s) {struktury.add(s);}
+
+
+    @Override
+    public Iterator<Structure> iterator() {
+        return struktury.iterator();
+    }
+}
