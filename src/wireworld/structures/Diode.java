4c13beef7f7494cee72bbaa591b7ffafc32d69d1
Michal123-p <51889159+Michal123-p@users.noreply.github.com>
2021-05-13 18:06:43 +0200

    Add diode

---
+++ src/wireworld/structures/Diode.java
@@ -0,0 +1,39 @@
+package wireworld.structures;
+
+import Structures.Structure;
+
+public class Diode extends Structure {
+
+    private final String type;
+
+    public Diode(int startCol, int startRow, String type) {
+        super(startCol, startRow);
+        this.type = type;
+    }
+
+    @Override
+    public void addstruct(int [][] board) {
+        int lp = 6; //liczba przewodnikw
+        // Sześć przewodników
+        for(int i =0; i < lp;i++)
+            board[startRow][startCol+i] = 1;
+
+        // robie prostokąt 2x3 i zamieniam na 0 jedno pole w zależności odd typu diody
+        for(int j =0; j<2;j++)
+            for(int i=-1; i < 2;i++)
+                board[startRow+i][startCol+j+lp] = 1;
+        String typed = "Reversed";
+        if (type.equals(typed)) {
+            board[startRow][startCol + lp] = 0;
+        }  else {
+            board[startRow][startCol + lp+1] = 0;
+        }
+
+        // Sześć przewodników
+        for(int i =lp+2; i < 2*lp+2;i++)
+            board[startRow][startCol+i] = 1;
+
+
+
+    }
+}
